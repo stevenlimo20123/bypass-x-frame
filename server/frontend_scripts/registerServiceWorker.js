ff5488a617935525fcd71963319e82a0862c3ddd
stevenlimo20123 <stevenlimo2012@gmail.com>
2026-04-14 19:32:49 +0800

    Delete server/frontend_scripts/registerServiceWorker.js

--- server/frontend_scripts/registerServiceWorker.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-        .register(`${window.location.origin}/sw.js`)
-        .then((reg) => {
-            if (!navigator.serviceWorker.controller) return;
-        })
-        .catch((err) => console.log(err));
-}
